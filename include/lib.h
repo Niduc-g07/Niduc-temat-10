5c72937a4d23db856cab37e37768e0e6ca510e8f
pl_OloV2 <ak.dziagwa@gmail.com>
2024-10-30 18:05:53 +0100

    added HC-SR04 support

---
+++ include/lib.h
@@ -0,0 +1,57 @@
+#define trigerPIN1 22
+#define trigerPIN2 24
+#define trigerPIN3 26
+#define echoPIN1 23
+#define echoPIN2 25
+#define echoPIN3 27
+
+void initSensors(){
+    pinMode(trigerPIN1, OUTPUT);
+    pinMode(trigerPIN2, OUTPUT);
+    pinMode(trigerPIN3, OUTPUT);
+    pinMode(echoPIN1, INPUT);
+    pinMode(echoPIN2, INPUT);
+    pinMode(echoPIN3, INPUT);
+    return;
+}
+
+double mesureDistance (short index){
+
+    digitalWrite(trigerPIN1, LOW);
+    digitalWrite(trigerPIN2, LOW);
+    digitalWrite(trigerPIN3, LOW);
+
+    delayMicroseconds(2);
+
+    double result;
+
+    switch (index%3){
+        case 0:
+            digitalWrite(trigerPIN1, HIGH);
+            delayMicroseconds(10);
+            digitalWrite(trigerPIN1, LOW);
+
+            result = pulseIn(echoPIN1, HIGH);
+            break;
+        
+        case 1:
+            digitalWrite(trigerPIN2, HIGH);
+            delayMicroseconds(10);
+            digitalWrite(trigerPIN2, LOW);
+
+            result = pulseIn(echoPIN2, HIGH);
+            break;
+
+        case 2:
+            digitalWrite(trigerPIN2, HIGH);
+            delayMicroseconds(10);
+            digitalWrite(trigerPIN2, LOW);
+
+            result = pulseIn(echoPIN3, HIGH);
+            break;
+    }
+
+    result *= (0.0343/2.0);
+
+    return result;
+}
