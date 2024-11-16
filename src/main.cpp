792ff7f52ccfd0a8a095979cfd67cc160b9231c9
Kacper <kacpermik529@gmail.com>
2024-11-16 03:20:23 +0100

    implement a TMR and median algorithms

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <Arduino.h>
 #include "lib.h"
+#include "TMR.h"
+#include "Median.h"
 
 void setup() {
   // put your setup code here, to run once:
-
   initSensors();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  mesureDistance(2);
+  majority_vote(mesureDistance(1),mesureDistance(2),mesureDistance(3));
+  find_median_sorted(mesureDistance(1),mesureDistance(2),mesureDistance(3));
+  
 }
 
 // put function definitions here:
