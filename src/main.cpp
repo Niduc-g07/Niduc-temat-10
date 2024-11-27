1e9f4ea101fe2145763ae1d28bdb9460bdce7a31
Filip <filipkwiek888@gmail.com>
2024-11-28 00:45:13 +0100

    Add iterative algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "lib.h"
 #include "TMR.h"
 #include "Median.h"
+#include "Iterative.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,7 +13,9 @@ void loop() {
   // put your main code here, to run repeatedly:
   majority_vote(mesureDistance(1),mesureDistance(2),mesureDistance(3));
   find_median_sorted(mesureDistance(1),mesureDistance(2),mesureDistance(3));
-  
+  iterative_vote(mesureDistance(1),mesureDistance(2),mesureDistance(3));
+
+  delay(100);
 }
 
 // put function definitions here:
