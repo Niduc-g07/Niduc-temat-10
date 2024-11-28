6ed35870d272ca03718eec8a4432e737b481246d
pl_OloV2 <ak.dziagwa@gmail.com>
2024-11-28 01:34:41 +0100

    wypisywanie do konsoli wyników

--- src/main.cpp
+++ src/main.cpp
@@ -16,16 +16,24 @@ void setup() {
 void loop() {
   data = allDistances();                            //szczytanie pomiaru z czujników
 
-  Serial.print("Glosowanie 1: ");                   //wypisanie wyniku algorytmu ?
+  Serial.print("Wyniki pomiarow:\n1. ");            //wypisanie wynikow do konsoli
+  Serial.print(data[0]);
+  Serial.print("cm\n2. ");
+  Serial.print(data[1]);
+  Serial.print("cm\n3. ");
+  Serial.print(data[2]);
+  Serial.print("cm\n");
+
+  Serial.print("Algorytm ?: ");                     //wypisanie wyniku algorytmu ?
   Serial.println(majority_vote(data[0], data[1], data[2]));
 
-  Serial.print("Glosowanie 2: ");                   //wypisanie wyniku algorytmu ?
+  Serial.print("Algorytm ?: ");                     //wypisanie wyniku algorytmu ?
   Serial.println(find_median_sorted(data[0], data[1], data[2]));
   
-  Serial.print("Glosowanie 3: ");                   //wypisanie wyniku algorytmu ?
+  Serial.print("Algorytm ?: ");                     //wypisanie wyniku algorytmu ?
   Serial.println(three_domain_voters(data[0], data[1], data[2]));
 
-  Serial.print("Glosowanie 4: ");                   //wypisanie wyniku algorytmu ?
+  Serial.print("Algorytm ?: ");                     //wypisanie wyniku algorytmu ?
   Serial.println(iterative_vote(data[0], data[1], data[2]));
 
   delay(2000);
